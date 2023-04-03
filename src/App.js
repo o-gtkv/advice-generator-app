7f4a46462c792ba33a840e77582a8161f972c365
Oleg <oleg_g87@outlook.com>
2023-04-03 21:38:47 +0300

    full refactoring

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import RandomAdviceBox from './components/RandomAdviceBox/RandomAdviceBox'
 import style from './App.module.css'
 
 function App() {
-	return (
-		<main className={style.main}>
-			<h1 className="sr-only">Advice generator</h1>
-			<RandomAdviceBox />
-		</main>
-	)
+    return (
+        <main className={style.main}>
+            <h1 className="sr-only">Advice generator</h1>
+            <RandomAdviceBox />
+        </main>
+    )
 }
 
 export default App;
